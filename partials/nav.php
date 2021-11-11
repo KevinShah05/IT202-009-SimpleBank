5be29312d6931b620eaa7d0c8a6490570490742b
KevinShah05 <90395978+KevinShah05@users.noreply.github.com>
2021-11-11 15:51:02 -0500

    UPDATED

--- partials/nav.php
+++ partials/nav.php
@@ -28,21 +28,6 @@ require(__DIR__ . "/../lib/functions.php");
 
 <nav>
 
-    <ul>
-        <?php if (is_logged_in()) : ?>
-            <li><a href="home.php">Home</a></li>
-        <?php endif; ?>
-        <?php if (!is_logged_in()) : ?>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
-        <?php endif; ?>
-        <?php if (has_role("Admin")) : ?>
-            <li><a href="#">Admin Stuff</a></li>
-        <?php endif; ?>
-        <?php if (is_logged_in()) : ?>
-            <li><a href="logout.php">Logout</a></li>
-        <?php endif; ?>
-    </ul>
 
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
